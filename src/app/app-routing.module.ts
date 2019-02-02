692cd7db32bcf6bcac5b30abaf42f8263d265319
nbaez <nbaez01@hotmail.com>
2019-02-02 14:35:52 -0500

    commit nbaez

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmpleadoComponent } from './empleado/empleado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"**", component:EmpleadoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
